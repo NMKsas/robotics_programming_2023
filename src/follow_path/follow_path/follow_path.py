a99b2270bbdc7e0ad72f9b89d3d96edb77025ebd
NMKsas <75199661+NMKsas@users.noreply.github.com>
2023-11-01 01:36:55 +0200

    Added simple menu for adding new paths

--- src/follow_path/follow_path/follow_path.py
+++ src/follow_path/follow_path/follow_path.py
@@ -5,8 +5,68 @@ import os
 # Goal constants
 DEFINED_PATHS_DIRECTORY = "../resource/"
 
+QUIT = 1
+INVALID_VALUE = "Invalid"
+RETURN_LIST = 3
 
-def main(args=None):
+
+def is_return_signal(input_value):
+    string_input = str(input_value)
+
+    if string_input.upper() == "R":
+        print("Returning the coordinate list...")
+        return True
+    return False
+
+
+def is_exit_signal(input_value):
+    string_input = str(input_value)
+
+    if string_input.upper() == "Q":
+        print("Quitting the program...")
+        return True
+    return False
+
+
+def validate_coordinate(input_value):
+    try:
+        coord = float(input_value)
+        return coord
+    except ValueError:
+        print("Invalid value '" + input_value + "'! Give numeric values (e.g., 2.0)")
+        return INVALID_VALUE
+
+
+def enter_points_manually():
+    print("-----------------------------")
+    print("Entering coordinates manually")
+    print("-----------------------------")
+    path_points_list = []
+    while True:
+        print()
+        print("Enter Q to quit, R to return the current list")
+        print("Current coordinate list: " + str(path_points_list) + "\n")
+
+        x_coord = input("Enter x-coordinate: ")
+        if is_exit_signal(x_coord):
+            return QUIT
+        if is_return_signal(x_coord):
+            return path_points_list
+
+        y_coord = input("Enter y-coordinate: ")
+        if is_exit_signal(y_coord):
+            return QUIT
+        if is_return_signal(y_coord):
+            return path_points_list
+
+        # continue to validate the inputs
+        validated_x = validate_coordinate(x_coord)
+        validated_y = validate_coordinate(y_coord)
+        if validated_x != INVALID_VALUE and validated_y != INVALID_VALUE:
+            path_points_list.append((validated_x, validated_y))
+
+
+def main():
     directory_entries = os.listdir(DEFINED_PATHS_DIRECTORY)
 
     while True:
@@ -14,13 +74,16 @@ def main(args=None):
         print("Enter F to read a file containing the path points")
         print("Enter Q to quit")
         choose_insertion_method = input()
+        print()
         if choose_insertion_method.upper() == "M":
-            print("Enter manually")
+            list_of_coordinates = enter_points_manually()
+            if list_of_coordinates == QUIT:
+                return
             break
         elif choose_insertion_method.upper() == "F":
             print("Enter from a file")
             break
-        elif choose_insertion_method.upper() == "Q":
+        elif is_exit_signal(choose_insertion_method):
             return
         else:
             continue
